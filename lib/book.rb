06aad5eb4fa473c855246a30d2dde82917ea98b9
Will Figueiredo <will@wfigueiredo.com>
2018-10-25 02:27:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,6 @@
-# Make your shoe class here!
 class Book
+  attr_accessor :author, :page_count, :genre
+
   def initialize(title)
     @title = title
   end
@@ -8,34 +9,8 @@ class Book
     @title
   end
 
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-end
 
-book = Book.new("Some Title")
-book.turn_page
+end
