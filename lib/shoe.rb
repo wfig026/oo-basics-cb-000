df72da85e458904444508459d2820440ab247098
Will Figueiredo <will@wfigueiredo.com>
2018-10-25 02:30:21 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,41 +1,11 @@
-# Make your shoe class here!
-class Book
-  def initialize(title)
-    @title = title
+class shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
   end
 
-  def title
-    @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  def cobble
+    puts "The shoe has been repaird."
 end
-
-book = Book.new("Some Title")
-book.turn_page
